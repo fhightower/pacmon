cde0dd63ee757038b89245620185d02e81182684
Floyd Hightower <floyd.hightower27@gmail.com>
2018-08-21 05:44:11 -0300

    Bump version: 0.1.0 → 0.2.0

--- pacmon/__init__.py
+++ pacmon/__init__.py
@@ -3,4 +3,4 @@
 
 __author__ = """Floyd Hightower"""
 __email__ = 'floyd.hightower27@gmail.com'
-__version__ = '0.1.0'
+__version__ = '0.2.0'
--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ test_requirements = [
 
 setup(
     name='pacmon',
-    version='0.1.0',
+    version='0.2.0',
     description="Software package monitoring system which sends alerts when a package has been changed.",
     long_description=readme,
     author="Floyd Hightower",
